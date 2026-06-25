2b8b8b563f98523bc79f60f0fd69bb2639646005
Ky Harrison <ky.harrison@cgwabrasives.com>
2026-06-25 15:13:30 -0500

    initial commit: mirror and sales history scripts

---
+++ bc-mirror/mirror.py
@@ -0,0 +1,166 @@
+from bc_keyvault_auth import build_bc_api_base_url, build_bc_odata_base_url, get_bc_connection, raise_bc_api_error
+import requests
+import sys
+import sqlite3
+import json
+import argparse
+from datetime import datetime
+import pathlib
+
+BC_CONFIG, BC_HEADERS = get_bc_connection()
+CONN = sqlite3.connect("bc_mirror.db")
+
+def parse_response(response):
+    data_raw = response.json()
+    data = [{key: value for key, value in record.items()
+             if not key.startswith("@")}
+             for record in data_raw["value"]]
+    next_link = data_raw.get("@odata.nextLink")
+    return data, next_link
+
+def call_api(endpoint, api_type):
+    print(f"Contacting server for endpoint: {endpoint} . . . ")
+    if api_type == "base":
+        response = requests.get(f"{build_bc_api_base_url(BC_CONFIG)}/{endpoint}", headers=BC_HEADERS)
+    elif api_type == "odata":
+        response = requests.get(f"{build_bc_odata_base_url(BC_CONFIG)}/{endpoint}", headers=BC_HEADERS)
+    else:
+        print('api_type must be defined as "base" or "odata"')
+        sys.exit(1)
+    if response.ok:
+        return response
+    else:
+        print(response.text)
+        sys.exit(1)
+
+def initialize_table(api_type, refresh_mode, endpoint, id_column, secondary_id=None, watermark_column=None, watermark_type=None, table_filter=None):
+    data, _ = parse_response(call_api(f"{endpoint}?$top=1", api_type))
+    keys = data[0].keys()
+    if secondary_id:
+        fields = ", ".join(keys) + f", PRIMARY KEY ({id_column}, {secondary_id})"
+    else:
+        fields = ", ".join([k + " PRIMARY KEY" if k == id_column else k for k in keys])
+    CONN.execute(f"CREATE TABLE IF NOT EXISTS {endpoint} ({fields})")
+    schema = {
+        "endpoint": endpoint, 
+        "api_type": api_type, # base | odata
+        "refresh_mode": refresh_mode, # upsert | truncate
+        "watermark_column": watermark_column, # column name
+        "watermark_type": watermark_type, # string | datetime
+        "id_column": id_column, 
+        "secondary_id": secondary_id,
+        "table_filter": table_filter
+        }
+    with open(f"schema/{endpoint}.json", "w") as f:
+        json.dump(schema, f, indent=2)
+
+def refresh_table(schema): 
+    
+    start = datetime.now()
+
+    endpoint = schema["endpoint"]
+    api_type = schema["api_type"]
+    refresh_mode = schema.get("refresh_mode")
+    watermark_column = schema["watermark_column"]
+    watermark_type = schema["watermark_type"]
+    table_filter = schema.get("table_filter")
+
+    watermark = None
+    if watermark_column:
+        check_watermark = CONN.execute(f"SELECT latest_diff FROM watermark WHERE endpoint = '{endpoint}'").fetchone()
+        watermark = check_watermark[0] if check_watermark else None
+
+    if watermark:
+        if watermark_type == "datetime":
+            data, next_link = parse_response(call_api(f"{endpoint}?$filter={watermark_column} ge {watermark}", api_type))
+        elif watermark_type == "string":
+            data, next_link = parse_response(call_api(f"{endpoint}?$filter={watermark_column} ge '{watermark}'", api_type))
+        else:
+            print(f"Schema must define watermark_type. Please check {endpoint}.json and try again.")
+            sys.exit(1)
+    elif table_filter:
+        data, next_link = parse_response(call_api(f"{endpoint}?$filter={table_filter}", api_type))
+    else:
+        data, next_link = parse_response(call_api(endpoint, api_type))
+    keys = data[0].keys()
+    number = len(keys)
+    fields = ", ".join(keys)
+
+    if refresh_mode == "truncate":
+        CONN.execute(f"DELETE FROM {endpoint}")
+        CONN.commit()
+
+    i = 0
+    count = 0
+    while True:
+        i = i + 1
+        records = [tuple(record.values()) for record in data]
+        count = count + len(records)
+        CONN.executemany(f"INSERT OR REPLACE INTO {endpoint} ({fields}) VALUES ({', '.join('?' * number)})", records)
+        CONN.commit()
+        print(f"{endpoint} written to file — page {i}")
+        if next_link:
+            data, next_link = parse_response(requests.get(next_link, headers=BC_HEADERS))
+            continue
+        else:
+            break
+    timestamp = datetime.now().isoformat()
+
+    if watermark_column:
+        watermark = CONN.execute(f"SELECT MAX({watermark_column}) FROM {endpoint}").fetchone()[0]
+    values = (endpoint, watermark, timestamp)
+    CONN.execute(f"INSERT OR REPLACE INTO watermark (endpoint, latest_diff, last_refreshed) VALUES (?, ?, ?)", values)
+    CONN.commit()
+    print(f"{endpoint} refreshed: {count} records updated in {datetime.now() - start}")
+
+
+if __name__ == "__main__":
+
+    start = datetime.now()
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="mode")
+
+    test = subparsers.add_parser("test")
+    test.add_argument("--api_type", required=True)
+    test.add_argument("--endpoint", required=True)
+
+    new = subparsers.add_parser("new")
+    new.add_argument("--api_type", required=True)
+    new.add_argument("--refresh_mode", required=True)
+    new.add_argument("--endpoint", required=True)
+    new.add_argument("--id_column", required=True)
+    new.add_argument("--secondary_id")
+    new.add_argument("--watermark_column") # omit for full refresh every time
+    new.add_argument("--watermark_type") # omit for full refresh every time
+    new.add_argument("--table_filter") # DO NOT COMBINE WITH WATERMARK
+
+    refresh = subparsers.add_parser("refresh")
+    refresh.add_argument("--endpoint")
+
+    args = parser.parse_args()
+
+    if args.mode == "test": 
+        top = parse_response(call_api(args.endpoint, args.api_type))
+        for line in top[0]:
+            print(json.dumps(line))
+
+    if args.mode == "new": 
+        initialize_table(args.api_type, args.refresh_mode, args.endpoint, args.id_column, args.secondary_id, args.watermark_column, args.watermark_type, args.table_filter) 
+
+    if args.mode == "refresh":  
+        if args.endpoint:
+            with open(pathlib.Path("schema") / f"{args.endpoint}.json") as f:
+                schema = json.load(f)
+            refresh_table(schema)
+        else:
+            for file in pathlib.Path("schema").glob("*.json"):
+                try:    
+                    with open(file) as f:
+                        schema = json.load(f)
+                        refresh_table(schema)
+                except Exception as e:
+                    print(f"{file.stem} failed, skipping to next table. Wait for refresh to finish before debugging.")
+                    print(e)
+    
+    print(f"Completed in {datetime.now() - start}")
---
+++ sales_history.py
@@ -0,0 +1,103 @@
+import sqlite3
+import argparse
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import os
+import numpy as np
+
+CONN = sqlite3.connect("bc_mirror.db")
+
+def case_when(period_start):
+    period_end = period_start + relativedelta(months=1)
+    label = period_start.strftime("%b %Y")
+    return (
+        f"SUM(CASE WHEN headers.Posting_Date >= '{period_start.strftime('%Y-%m-%d')}' "
+        f"AND headers.Posting_Date < '{period_end.strftime('%Y-%m-%d')}' "
+        f"THEN lines.Quantity ELSE 0 END) AS \"{label}\""
+    )
+
+def run():
+    now = datetime.now()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--items")
+    parser.add_argument("--customer")
+    parser.add_argument("--location")
+
+    """
+    parser.add_argument("--start") 
+    parser.add_argument("--grain") 
+    parser.add_argument("--periods") # TODO later add month-to-date
+    parser.add_argument("--uom")
+    parser.add_argument("--state")
+    """
+    args = parser.parse_args()
+
+    start = (now.replace(day=1) - relativedelta(months=12))
+    periods = 12
+    end = start + relativedelta(months=periods)
+
+    lines = []
+    lines.append("SELECT lines.No, ")
+
+    dates = []
+    for i in range(periods):
+        dates.append(case_when(start + relativedelta(months=i)))
+    lines.append(", ".join(dates))
+
+    lines.append("FROM Posted_Sales_Invoice_Lines AS lines")
+    lines.append("JOIN Posted_Sales_Invoices_Header AS headers")
+    lines.append("ON lines.Document_No = headers.No")
+    lines.append(f"WHERE headers.Posting_Date >= '{start.strftime('%Y-%m-%d')}'")
+    lines.append(f"AND headers.Posting_Date < '{end.strftime('%Y-%m-%d')}'")
+    lines.append("AND lines.Type = 'Item'")
+    if args.items:
+        items = args.items.split("|") 
+        item_list = ", ".join(f"'{i}'" for i in items)
+        lines.append(f"AND lines.No IN ({item_list})")
+    if args.customer:
+        lines.append(f"AND lines.Sell_to_Customer_No = '{args.customer}'")
+    if args.location:
+        lines.append(f"AND headers.Location_Code = '{args.location}'")
+    lines.append("GROUP BY lines.No")
+
+    query = " \n".join(lines)
+
+    df = pd.read_sql(query, CONN)
+
+    last_12m = df.columns[1:]
+    df["12m_avg"] = df[last_12m].mean(axis=1).round(0).astype(int)
+    last_3m = last_12m[-3:]
+    df["3m_avg"] = df[last_3m].mean(axis=1).round(0).astype(int)
+    df["12m_peak"] = df[last_12m].max(axis=1)
+    df["3m_peak"] = df[last_3m].max(axis=1)
+    df["trend"] = (df["3m_avg"] / df["12m_avg"]).round(1)
+    df["cv"] = (df[last_12m].std(axis=1) / df["12m_avg"]).round(1)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df = df.sort_values('12m_avg', ascending=False)
+
+    filename = f"sales-history-{now.strftime('%y-%m-%d')}.xlsx"
+    
+    with pd.ExcelWriter(filename, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sales History')
+        workbook = writer.book
+        worksheet = writer.sheets['Sales History']
+        num_format = workbook.add_format({'num_format': '_(* #,##0_);_(* (#,##0);_(* "-"??_);_(@_)'})
+        pct_format = workbook.add_format({'num_format': '0%'})
+        worksheet.add_table(0, 0, len(df), len(df.columns)-1, {
+            'style': 'None',
+            'columns': [
+                {'header': df.columns[0]}
+            ] + [
+                {'header': col} for col in df.columns[1:]
+            ]
+        })
+        worksheet.set_column(1, len(df.columns)-3, None, num_format)
+        worksheet.set_column(len(df.columns)-1, len(df.columns)-1, None, pct_format)
+
+    os.startfile(filename)
+
+
+if __name__ == "__main__":
+    print(run())
